0ada8a5da6900075cc01aedf6b0528db332142da
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2023-06-17 12:51:34 -0600

    Proyecto

---
+++ controllers/client-controller.js
@@ -0,0 +1,46 @@
+import { clientServices } from "../service/client-service.js";
+
+
+const crearNuevaLinea = (nombre, email, id) => {
+    const linea = document.createElement("tr")
+    const contenido = `
+            <td class="td" data-td>${nombre}</td>
+            <td>${email}</td>
+            <td>
+                <ul class="table__button-control">
+                    <li>
+                        <a
+                            href="../screens/editar_cliente.html?id=${id}"
+                            class="simple-button simple-button--edit">Editar</a>
+                    </li>
+                    <li>
+                        <button
+                            class="simple-button simple-button--delete"
+                            type="button" id="${id}">Eliminar
+                        </button>
+                    </li>
+                </ul>
+            </td>
+    `;
+    linea.innerHTML = contenido;
+    const btn = linea.querySelector("button");
+    btn.addEventListener("click", () =>{
+        const id = btn.id
+        clientServices.eliminarCliente(id).then(respuesta =>{
+            console.log(respuesta);
+        }).catch(error => alert("No se pudo eliminar al cliente"))
+    })
+    console.log(btn);
+    return linea;
+};
+
+const table = document.querySelector("[data-table]");
+
+clientServices.listaClientes()
+    .then((data) => {
+        console.log(data);
+        data.forEach(({nombre,email,id}) => {
+            const nuevaLinea = crearNuevaLinea(nombre,email,id);
+            table.appendChild(nuevaLinea);
+        });
+    }).catch((error) => alert("Ocurrio un error ):"));
